72d24cbf3051583633b9f9c7869a5f9ec5530bb9
zouheir <56370066+zouheir023@users.noreply.github.com>
2023-09-02 13:59:12 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,17 +1,4 @@
 <?php
-/*
-|--------------------------------------------------------------------------
-| OWSA-INV V2
-|--------------------------------------------------------------------------
-| Author: Siamon Hasan
-| Project Name: OSWA-INV
-| Version: v2
-| Offcial page: http://oswapp.com/
-| facebook Page: https://www.facebook.com/oswapp
-|
-|
-|
-*/
   define( 'DB_HOST', 'localhost' );          // Set database host
   define( 'DB_USER', 'root' );             // Set database user
   define( 'DB_PASS', '123456' );             // Set database password
